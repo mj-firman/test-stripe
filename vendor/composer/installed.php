53c620302548659d0b80e97efc25da196a4cfc48
mj-firman <firman@magic-jungle.co.id>
2021-12-15 11:50:14 +0700

    kedua

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e11f5a1739bee8089276d67c817fd8063765cfe2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e11f5a1739bee8089276d67c817fd8063765cfe2',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v6.43.1',
+            'version' => '6.43.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '42fcdaf99c44bb26937223f8eae1f263491d5ab8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
